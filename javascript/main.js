e5d3d0ad9c9658f93a26fac2640e51d23a7219f1
Dloft <derek.lofton1@gmail.com>
2018-12-24 15:45:41 -0500

    javastart

---
+++ javascript/main.js
@@ -0,0 +1,32 @@
+var game = document.querySelector('main');
+
+var money = {
+    penny: {
+        name: "penny",
+        value: .01
+    },
+
+    nickle: {
+        name: "nickle",
+        value: .05
+    },
+
+    dime: {
+        name: "dime",
+        value: .10
+    },
+    
+    quarter: {
+        name: "quarter",
+        value: .25
+    },
+
+    halfDollar: {
+        name: "half dollar",
+        value: .50
+    },
+
+    
+
+
+};
